53693f91b494dca674df863c365d33ee493fcc7a
Elizaveta Plokhotnikova <lizapd@icloud.com>
2022-07-12 12:41:20 +0700

    Add first a task

---
+++ Lesson/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1, num2;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
+
